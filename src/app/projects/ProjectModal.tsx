b8fbe32cb4360b5aa4026399c5e04d2339a82795
Steven Le <stevenleusa79@yahoo.com>
2024-01-25 02:37:46 -0800

    should fix mobile project modal wrapping(?)

--- src/app/projects/ProjectModal.tsx
+++ src/app/projects/ProjectModal.tsx
@@ -30,7 +30,7 @@ export default function ProjectModalComponent({photoUrl, title, dateInfo, descri
     <div className="col-span-5 gap-0 text-center">
       <button className="bg-gray-700 rounded-lg text-black text-sm p-2 border:none hover:bg-gray-600" onClick={()=>toggle(true)}>View more...</button>
       {<dialog ref={ref} className={classNames("modal", {"modal-open": isToggled})}>
-          <div className="modal-box bg-[#7C909A] text-black text-wrap max-w-[100vw] md:w-[70%] flex flex-col gap-4">
+          <div className="modal-box bg-[#7C909A] text-black text-wrap max-w-screen md:max-w-none md:w-[70%] flex flex-col gap-4">
             <div className="flex flex-col gap-4 text-left">
               <h3 className="font-bold text-4xl">{title}</h3>
               <div className="text-sm">{dateInfo} - <Link href={repoUrl} target="_blank" rel="noopener noreferrer" className="font-bold hover:text-cyan-400">VIEW ON GITHUB</Link></div>
