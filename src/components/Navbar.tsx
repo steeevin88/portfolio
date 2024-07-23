24a551a119d0a4c2f4713972eb968333b0d0484b
Steven Le <stevenleusa79@yahoo.com>
2024-07-22 20:07:14 -0700

    update resume link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,7 +23,7 @@ export default function Navbar() {
         <Link href="/experience" onClick={() => setToggled(!toggled)}>Experience</Link>
         <Link href="/projects" onClick={() => setToggled(!toggled)}>Projects</Link>
       </div>
-      <Link href="https://drive.google.com/file/d/1M__DCQg2Ix214ksZW0z4fGsBXHDieqXC/view?usp=drive_link" target="_blank" rel="noopener noreferrer"><button className="btn rounded-lg">Resume</button></Link>
+      <Link href="https://drive.google.com/file/d/18GNlxTva-8pbxtfJufObDr2tN-0hhnLa/view?usp=drive_link" target="_blank" rel="noopener noreferrer"><button className="btn rounded-lg">Resume</button></Link>
     </nav>
   )
 }
