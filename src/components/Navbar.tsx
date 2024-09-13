172836f36262611232930581d2cdf2e82cc2a69e
Steven Le <stevenleusa79@yahoo.com>
2024-09-13 15:22:55 -0700

    minor nav styling fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,6 +12,7 @@ export default function Navbar() {
       <div className="flex items-center lg:hidden" onClick={() => setToggled(!toggled)}>
         <IoMenu className="h-10 w-10"/>
       </div>
+      {/* Social Icons */}
       <div className="hidden lg:flex flex-row gap-4">
         <Link href="https://www.linkedin.com/in/steven-le-90b28b23b/" target="_blank" rel="noopener noreferrer">
           <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24" fill="#000000"><path fill="#000000" d="M20 3H4a1 1 0 0 0-1 1v16a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1M8.339 18.337H5.667v-8.59h2.672zM7.003 8.574a1.548 1.548 0 1 1 0-3.096a1.548 1.548 0 0 1 0 3.096m11.335 9.763h-2.669V14.16c0-.996-.018-2.277-1.388-2.277c-1.39 0-1.601 1.086-1.601 2.207v4.248h-2.667v-8.59h2.56v1.174h.037c.355-.675 1.227-1.387 2.524-1.387c2.704 0 3.203 1.778 3.203 4.092v4.71z"></path></svg>
@@ -24,7 +25,8 @@ export default function Navbar() {
         </Link>
 
       </div>
-      <div className="flex justify-end flex-col gap-4 mt-3 mb-8 lg:flex-row lg:gap-12 lg:mt-0">
+      {/* Nav Links */}
+      <div className="flex justify-end flex-col gap-4 mt-3 mb-8 lg:flex-row lg:gap-12 lg:mt-1">
         <Link href="#home" onClick={() => setToggled(!toggled)}>Home</Link>
         <Link href="#about" onClick={() => setToggled(!toggled)}>About</Link>
         <Link href="#experience" onClick={() => setToggled(!toggled)}>Experience</Link>
