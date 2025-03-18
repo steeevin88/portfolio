045b81a02003bce5a629782649b85d3f60245ca0
Steven Le <stevenleusa79@yahoo.com>
2025-03-17 22:07:45 -0700

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,10 +36,10 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <section className="flex flex-col justify-center items-center min-h-screen py-8 px-4 bg-gradient-to-b from-[#7C909A] via-[#7C909A] to-gray-300" id="home">
-        <div className="text-6xl md:text-7xl lg:text-8xl my-8 h-32 text-center">
-          <Typewriter onInit={(typewriter) => {typewriter.typeString('Hey! I\'m Steven.').start();}}/>
+        <div className="text-5xl md:text-7xl lg:text-8xl font-bold text-center my-8 h-32 whitespace-nowrap overflow-hidden">
+          <Typewriter onInit={(typewriter) => {typewriter.typeString("Hey! I'm Steven.").start()}} />
         </div>
-        <div className="text-lg sm:text-xl md:text-3xl mb-4 text-center">CS Major @ San Jose State University, aspiring software engineer</div>
+
         <div className="text-lg sm:text-xl md:text-3xl mb-4 text-center">As of right now, I am probably...</div>
         <div className="text-2xl sm:text-3xl md:text-5xl h-10 md:h-12 my-12 md:my-16 text-center">
           <p>{randomText}</p>
